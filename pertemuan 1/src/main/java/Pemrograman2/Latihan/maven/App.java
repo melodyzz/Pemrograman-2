a8910b3a15143f865472311f1bfbcd8bbd910856
viorra22 <virgie.sciorra@gmail.com>
2013-04-08 16:05:16 +0700

    pert2

---
+++ pertemuan 1/src/main/java/Pemrograman2/Latihan/maven/App.java
@@ -0,0 +1,13 @@
+package Pemrograman2.Latihan.maven;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
---
+++ pertemuan 1/src/main/java/Pemrograman2/Latihan/maven/halo.java
@@ -0,0 +1,9 @@
+package Pemrograman2.Latihan.maven;
+
+public class halo{
+        public static void main (String[] xx){
+		
+		System.out.println("halo maven");
+		}
+}
+		
